6079297da05a5d12b7417de7a17338ace20b4305
Dmitriy Korobeinikov <Dmitriy_Korobeinikov@epam.com>
2016-02-19 16:20:39 +0400

    Added three node in the row and made good size for them.

--- app/src/main/java/com/epam/dmitriy_korobeinikov/graphickeyview/GraphicKeyNode.java
+++ app/src/main/java/com/epam/dmitriy_korobeinikov/graphickeyview/GraphicKeyNode.java
@@ -1,7 +1,9 @@
 package com.epam.dmitriy_korobeinikov.graphickeyview;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.ImageButton;
 
 /**
@@ -15,5 +17,12 @@ public class GraphicKeyNode extends ImageButton {
 
     public GraphicKeyNode(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Drawable drawable = getResources().getDrawable(R.drawable.key_node_pressed);
+        setBackground(drawable);
+        return false;
     }
 }
