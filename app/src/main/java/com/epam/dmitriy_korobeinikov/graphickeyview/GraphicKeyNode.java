021c31e9d6973c2ef6da92c9870392b88ee01998
Dmitriy Korobeinikov <Dmitriy_Korobeinikov@epam.com>
2016-02-19 18:19:18 +0400

    Started to add states for nodes

--- app/src/main/java/com/epam/dmitriy_korobeinikov/graphickeyview/GraphicKeyNode.java
+++ app/src/main/java/com/epam/dmitriy_korobeinikov/graphickeyview/GraphicKeyNode.java
@@ -2,6 +2,7 @@ package com.epam.dmitriy_korobeinikov.graphickeyview;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.IntDef;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.ImageButton;
@@ -10,6 +11,15 @@ import android.widget.ImageButton;
  * Created by Dmitriy_Korobeinikov on 2/19/2016.
  */
 public class GraphicKeyNode extends ImageButton {
+
+    public static final int STATE_DEFAULT = 0;
+    public static final int STATE_PRESSED = 1;
+    public static final int STATE_WRONG_KEY = 2;
+
+    @IntDef({STATE_DEFAULT, STATE_PRESSED, STATE_WRONG_KEY})
+    public @interface KeyNodeState {
+
+    }
 
     public GraphicKeyNode(Context context) {
         super(context);
@@ -24,5 +34,16 @@ public class GraphicKeyNode extends ImageButton {
         Drawable drawable = getResources().getDrawable(R.drawable.key_node_pressed);
         setBackground(drawable);
         return false;
+    }
+
+    public int[] getCenterCoordinates() {
+        int[] center = new int[2];
+        center[0] = (int) (getX() + getWidth() / 2);
+        center[1] = (int) (getY() + getHeight() / 2);
+        return center;
+    }
+
+    public void updateStateDrawable(@KeyNodeState int state) {
+
     }
 }
--- app/src/main/java/com/epam/dmitriy_korobeinikov/graphickeyview/GraphicKeyView.java
+++ app/src/main/java/com/epam/dmitriy_korobeinikov/graphickeyview/GraphicKeyView.java
@@ -4,20 +4,28 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 /**
  * Created by Dmitriy_Korobeinikov on 2/19/2016.
  */
 public class GraphicKeyView extends RelativeLayout {
 
-    private Paint mPaint;
     private int startX;
     private int startY;
     private int endX;
     private int endY;
+
+    private ArrayList<float[]> mLines;
+
+    private Paint mPaint;
+    private GraphicKeyNode mNodeUnderTap;
 
     public GraphicKeyView(Context context) {
         super(context);
@@ -30,6 +38,7 @@ public class GraphicKeyView extends RelativeLayout {
 
     private void init() {
         mPaint = new Paint();
+        mLines = new ArrayList<>();
         mPaint.setAntiAlias(true);
         mPaint.setColor(Color.BLACK);
         mPaint.setStyle(Paint.Style.STROKE);
@@ -46,19 +55,43 @@ public class GraphicKeyView extends RelativeLayout {
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                startX = (int) event.getX();
-                startY = (int) event.getY();
-                endX = (int) event.getX();
-                endY = (int) event.getY();
+                if (isTapInsideNode(event)) {
+                    int[] center = mNodeUnderTap.getCenterCoordinates();
+                    startX = endX = center[0];
+                    startY = endY = center[1];
+                }
                 break;
             case MotionEvent.ACTION_MOVE:
-                endX = (int) event.getX();
-                endY = (int) event.getY();
+                if (mNodeUnderTap != null) {
+                    endX = (int) event.getX();
+                    endY = (int) event.getY();
+                    if (isTapInsideNode(event)) {
+
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                mNodeUnderTap = null;
                 break;
             default:
                 return false;
         }
         invalidate();
         return true;
+    }
+
+    private boolean isTapInsideNode(MotionEvent event) {
+        int childCount = getChildCount();
+        View view;
+        Rect rect = new Rect();
+        for (int i = 0; i < childCount; i++) {
+            view = getChildAt(i);
+            view.getHitRect(rect);
+            if (rect.contains((int) event.getX(), (int) event.getY())) {
+                mNodeUnderTap = (GraphicKeyNode) view;
+                return true;
+            }
+        }
+        return false;
     }
 }
