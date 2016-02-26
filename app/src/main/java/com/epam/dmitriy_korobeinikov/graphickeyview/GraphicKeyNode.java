5d8d6e86f200bf5bb4e6d76b68165d29ac0c68fe
Dmitriy Korobeinikov <Dmitriy_Korobeinikov@epam.com>
2016-02-26 16:25:35 +0400

    Raw commit for experiment.

--- app/src/main/java/com/epam/dmitriy_korobeinikov/graphickeyview/GraphicKeyNode.java
+++ app/src/main/java/com/epam/dmitriy_korobeinikov/graphickeyview/GraphicKeyNode.java
@@ -1,8 +1,14 @@
 package com.epam.dmitriy_korobeinikov.graphickeyview;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.IntDef;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
@@ -32,18 +38,6 @@ public class GraphicKeyNode extends View {
         super(context, attrs);
     }
 
-    public Point getCenter() {
-        return new Point(getCenterX(), getCenterY());
-    }
-
-    public int getCenterX() {
-        return (int) (getX() + getWidth() / 2);
-    }
-
-    public int getCenterY() {
-        return (int) (getY() + getHeight() / 2);
-    }
-
     public void updateState(@KeyNodeState int state) {
         Drawable drawable = null;
         switch (state) {
@@ -56,13 +50,45 @@ public class GraphicKeyNode extends View {
                 setPressed(true);
                 break;
             case STATE_WRONG_KEY:
-                drawable = ContextCompat.getDrawable(getContext(), R.drawable.key_node_wrong);
+                Bitmap bitmap = createFromLayerList(R.drawable.key_node_wrong_90);
+                drawable = rotateBitmap(bitmap);
                 break;
         }
         setBackground(drawable);
     }
 
+    public Bitmap createFromLayerList(@DrawableRes int res) {
+        LayerDrawable layerDrawable = (LayerDrawable) ContextCompat.getDrawable(getContext(), res);
+        Bitmap bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+
+        layerDrawable.setBounds(0, 0, getWidth(), getHeight());
+        layerDrawable.draw(new Canvas(bitmap));
+        return bitmap;
+    }
+
+    public BitmapDrawable rotateBitmap(Bitmap original) {
+        Bitmap result = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+
+        Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
+        Canvas canvas = new Canvas(result);
+        canvas.rotate(mArrowAngle, getWidth() / 2, getHeight() / 2);
+        canvas.drawBitmap(original, 0, 0, paint);
+        return new BitmapDrawable(getResources(), result);
+    }
+
     public void setArrowAngle(float arrowAngle) {
         mArrowAngle = arrowAngle;
+    }
+
+    public Point getCenter() {
+        return new Point(getCenterX(), getCenterY());
+    }
+
+    public int getCenterX() {
+        return (int) (getX() + getWidth() / 2);
+    }
+
+    public int getCenterY() {
+        return (int) (getY() + getHeight() / 2);
     }
 }
